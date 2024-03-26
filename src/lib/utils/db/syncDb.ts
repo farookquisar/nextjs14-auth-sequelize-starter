9161bde65dd4b7e5415c148864077e9b2521bb7c
farookquisar <farook.quisar@gmail.com>
2024-03-26 13:40:04 +0300

    Update syncDb.ts

--- src/lib/utils/db/syncDb.ts
+++ src/lib/utils/db/syncDb.ts
@@ -10,27 +10,27 @@ export async function seedUsers() {
 	// Drop all users
 	await User.destroy({ truncate: true });
 
-	// const user1 = await User.create({
-	// 	firstName: 'John',
-	// 	lastName: 'Doe',
-	// 	email: 'john@example.com',
-	// 	phone: '(555) 555-5555',
-	// 	password: 'jyhnuz-qaXnis-3vydfo', // the kind an idiot would have on his luggage
-	// 	avatar: 'https://cdn.designly.biz/fake-user/john.jpg',
-	// 	role: 'admin',
-	// 	status: 'active',
-	// });
+	const user1 = await User.create({
+		firstName: 'John',
+		lastName: 'Doe',
+		email: 'john@example.com',
+		phone: '(555) 555-5555',
+		password: 'jyhnuz-qaXnis-3vydfo', // the kind an idiot would have on his luggage
+		avatar: 'https://cdn.designly.biz/fake-user/john.jpg',
+		role: 'admin',
+		status: 'active',
+	});
 
-	// const user2 = await User.create({
-	// 	firstName: 'Jane',
-	// 	lastName: 'Doe',
-	// 	email: 'jane@example.com',
-	// 	phone: '(555) 555-5556',
-	// 	password: 'jyhnuz-qaXnis-3vydfocuT', // the kind an idiot would have on his luggage
-	// 	avatar: 'https://cdn.designly.biz/fake-user/jane.jpg',
-	// 	role: 'customer',
-	// 	status: 'active',
-	// });
+	const user2 = await User.create({
+		firstName: 'Jane',
+		lastName: 'Doe',
+		email: 'jane@example.com',
+		phone: '(555) 555-5556',
+		password: 'jyhnuz-qaXnis-3vydfocuT', // the kind an idiot would have on his luggage
+		avatar: 'https://cdn.designly.biz/fake-user/jane.jpg',
+		role: 'customer',
+		status: 'active',
+	});
 
 	const user3 = await User.create({
 		firstName: 'Next',
