8a3c6a3cb4dd9ecc317914d649fb237c6b42e9e6
farookquisar <farook.quisar@gmail.com>
2024-03-26 13:38:42 +0300

    Update syncDb.ts

--- src/lib/utils/db/syncDb.ts
+++ src/lib/utils/db/syncDb.ts
@@ -10,39 +10,39 @@ export async function seedUsers() {
 	// Drop all users
 	await User.destroy({ truncate: true });
 
-	const user1 = await User.create({
-		firstName: 'John',
-		lastName: 'Doe',
-		email: 'john@example.com',
-		phone: '(555) 555-5555',
-		password: 'jyhnuz-qaXnis-3vydfo', // the kind an idiot would have on his luggage
-		avatar: 'https://cdn.designly.biz/fake-user/john.jpg',
-		role: 'admin',
-		status: 'active',
-	});
-
-	const user2 = await User.create({
-		firstName: 'Jane',
-		lastName: 'Doe',
-		email: 'jane@example.com',
-		phone: '(555) 555-5556',
-		password: 'jyhnuz-qaXnis-3vydfocuT', // the kind an idiot would have on his luggage
-		avatar: 'https://cdn.designly.biz/fake-user/jane.jpg',
-		role: 'customer',
-		status: 'active',
-	});
-
-	// const user3 = await User.create({
-	// 	firstName: 'Next',
-	// 	lastName: 'User',
-	// 	email: 'user@nextmail.com',
-	// 	phone: '(555) 555-5556',
+	// const user1 = await User.create({
+	// 	firstName: 'John',
+	// 	lastName: 'Doe',
+	// 	email: 'john@example.com',
+	// 	phone: '(555) 555-5555',
 	// 	password: 'jyhnuz-qaXnis-3vydfo', // the kind an idiot would have on his luggage
+	// 	avatar: 'https://cdn.designly.biz/fake-user/john.jpg',
+	// 	role: 'admin',
+	// 	status: 'active',
+	// });
+
+	// const user2 = await User.create({
+	// 	firstName: 'Jane',
+	// 	lastName: 'Doe',
+	// 	email: 'jane@example.com',
+	// 	phone: '(555) 555-5556',
+	// 	password: 'jyhnuz-qaXnis-3vydfocuT', // the kind an idiot would have on his luggage
 	// 	avatar: 'https://cdn.designly.biz/fake-user/jane.jpg',
 	// 	role: 'customer',
 	// 	status: 'active',
 	// });
 
+	const user3 = await User.create({
+		firstName: 'Next',
+		lastName: 'User',
+		email: 'user@nextmail.com',
+		phone: '(555) 555-5556',
+		password: 'jyhnuz-qaXnis-3vydfo', // the kind an idiot would have on his luggage
+		avatar: 'https://cdn.designly.biz/fake-user/jane.jpg',
+		role: 'customer',
+		status: 'active',
+	});
 
-	return [user1, user2];
+
+	return [user3];
 }
